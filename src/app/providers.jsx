e70be67b995ed2898561bddbe696cc410923e50a
thinh <llllpppptttt2003@gmail.com>
2024-05-17 21:08:11 +0700

    Update commit

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -14,7 +14,7 @@ const AppInfo = {
 const config = getDefaultConfig({
     appName: 'My RainbowKit App',
     autoConnect: true,
-    projectId: projectId,
+    projectId: "f0e843dddcf3d3826b2f0f920a8b2917" || projectId,
     chains: [mainnet, sepolia, polygon, hardhat],
     transports: {
         [mainnet.id]: http(), 
