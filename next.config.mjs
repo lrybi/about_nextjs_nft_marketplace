92fdc309ce60f54c878e5fcc08f16b07fce414f5
thinh <llllpppptttt2003@gmail.com>
2024-05-17 21:41:10 +0700

    Update commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
+    reactStrictMode: false,
+    swcMinify: true,
+    images: {
+        loader: "akamai",
+        path: "",
+        domains: ["cdn.jsdelivr.net"],
+    },
     webpack: (config) => {
         config.resolve.fallback = { fs: false, net: false, tls: false };
         config.externals.push("pino-pretty", "lokijs", "encoding");
